d3141c85b0238e944ec2dc52c6f5f9ad226d5246
GrossTrevor <grosstrevor000@gmail.com>
2026-04-02 17:46:55 -0400

    feat: track execution times and generate graph

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import hvlcs
+import datetime
+import matplotlib.pyplot as plt
 
 def main():
     run_tests = True
@@ -11,11 +13,23 @@ def main():
     # Tests
     if run_tests:
         test_cases = 13
+        plt.figure(figsize=(10, 5))
+        times = []
         for i in range(1, test_cases + 1):
+            start_time = datetime.datetime.now()
             K, vals, A, B = parse(f'tests/test_{i}.in')
             max_value, lcs = hvlcs.hvlcs(K, vals, A, B)
+            end_time = datetime.datetime.now()
+            times.append((end_time - start_time).total_seconds())
             with open(f'tests/test_{i}.out', 'w') as file:
                 file.write(f'{max_value}\n{lcs}')
+        plt.plot(range(1, test_cases + 1), times, marker='o')
+        plt.title('Execution Time for Test Cases')
+        plt.xlabel('Test Case')
+        plt.ylabel('Time (seconds)')
+        plt.xticks(range(1, test_cases + 1))
+        plt.grid()
+        plt.savefig('tests/execution_times.png')
     return 0
 
 def parse(file_path):
