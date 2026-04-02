e4c722592144781b24921e99afe7c0194c911692
GrossTrevor <grosstrevor000@gmail.com>
2026-04-02 17:35:26 -0400

    feat: add testing files and capability

--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
 import hvlcs
 
 def main():
+    run_tests = True
+
     K, vals, A, B = parse('data/example.in')
     max_value, lcs = hvlcs.hvlcs(K, vals, A, B)
     with open('data/output.out', 'w') as file:
         file.write(f'{max_value}\n{lcs}')
+
+    # Tests
+    if run_tests:
+        test_cases = 13
+        for i in range(1, test_cases + 1):
+            K, vals, A, B = parse(f'tests/test_{i}.in')
+            max_value, lcs = hvlcs.hvlcs(K, vals, A, B)
+            with open(f'tests/test_{i}.out', 'w') as file:
+                file.write(f'{max_value}\n{lcs}')
     return 0
 
 def parse(file_path):
